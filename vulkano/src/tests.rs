385997c00ae5a642d35ae20612383ed64bc35c9a
i509VCB <git@i509.me>
2021-06-08 00:36:15 -0500

    Toy with VK_EXT_physical_device_drm

--- vulkano/src/tests.rs
+++ vulkano/src/tests.rs
@@ -99,3 +99,97 @@ macro_rules! assert_should_panic {
         }
     }};
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::instance::{Instance, InstanceExtensions, RawInstanceExtensions, PhysicalDevice};
+    use crate::{Version, VulkanObject};
+    use std::ffi::CString;
+    use crate::device::{RawDeviceExtensions, Device, Features};
+    use std::{ptr, mem};
+    use ash::vk::PhysicalDeviceProperties;
+    use std::os::raw::c_void;
+    use std::mem::MaybeUninit;
+
+    #[test]
+    fn test() {
+        eprintln!("Test");
+        let extensions = InstanceExtensions {
+            khr_get_physical_device_properties2: true,
+            ..InstanceExtensions::none()
+        };
+
+        let instance = Instance::new(
+            None,
+            Version::V1_2,
+            &extensions,
+            None,
+            //["VK_LAYER_KHRONOS_validation"],
+        ).unwrap();
+
+        let count = PhysicalDevice::enumerate(&instance).count();
+        println!("{}", count);
+
+        let physical_device = PhysicalDevice::enumerate(&instance).nth(1).unwrap();
+
+        println!("{}", physical_device.name());
+
+        let extensions = RawDeviceExtensions::supported_by_device(physical_device);
+        let queue_family = physical_device.queue_families().find(|q| q.supports_graphics()).unwrap();
+
+        {
+            #[derive(Debug)]
+            #[repr(C)]
+            struct PhysicalDeviceDrmPropertiesEXT {
+                s_type: ash::vk::StructureType,
+                p_next: *mut c_void,
+                has_primary: ash::vk::Bool32,
+                has_render: ash::vk::Bool32,
+                primary_major: i64,
+                primary_minor: i64,
+                render_major: i64,
+                render_minor: i64,
+            }
+
+            let internal_physical_device = physical_device.internal_object();
+            let ext = unsafe { PhysicalDeviceDrmPropertiesEXT {
+                s_type: ash::vk::StructureType::from_raw(1000353000), // magic value
+                ..mem::zeroed()
+            } };
+            let mut ext = MaybeUninit::new(ext);
+
+            let mut properties = unsafe { ash::vk::PhysicalDeviceProperties2 {
+                s_type: ash::vk::StructureType::PHYSICAL_DEVICE_PROPERTIES_2_KHR,
+                p_next: (&mut ext).as_mut_ptr() as *mut c_void,
+                ..mem::zeroed()
+            } };
+
+            unsafe { physical_device.instance()
+                .clone()
+                .fns()
+                .khr_get_physical_device_properties2
+                .get_physical_device_properties2_khr(
+                    internal_physical_device,
+                    &mut properties
+                );
+            }
+
+            eprintln!("{:?}", properties);
+
+            let ext = properties.p_next as *mut PhysicalDeviceDrmPropertiesEXT;
+
+            if ext.is_null() {
+                panic!()
+            }
+
+            eprintln!("{:?}", unsafe { ext.as_ref().unwrap() });
+
+            // let (device, queues) = Device::new(
+            //     physical_device,
+            //     &Features::none(),
+            //     extensions.clone(),
+            //     [(queue_family, 0.5)].iter().cloned()
+            // ).unwrap();
+        }
+    }
+}
